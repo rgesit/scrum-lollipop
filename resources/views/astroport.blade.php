f7094ab02b8bd459710a0324908fb356a03b909c
Heri Gunawan <aageboi@gmail.com>
2015-12-14 14:59:51 +0700

    docker

--- resources/views/astroport.blade.php
+++ resources/views/astroport.blade.php
@@ -8,25 +8,24 @@
 
 		<h1>Lollipop</h1>
 
+		<h3>Ship</h3>
 		<p id="gate-1">
-			<h3>Ship</h3>
 			<a href="/astroport/ship-1" id="ship-1">An element that will display the name of the ship docked at gate 1</a>
 		</p>
 		<p id="gate-2">
-			<h3>Ship</h3>
 			<a href="/astroport/ship-2" id="ship-2">An element that will display the name of the ship docked at gate 2</a>
 		</p>
 		<p id="gate-3">
-			<h3>Ship</h3>
 			<a href="/astroport/ship-3" id="ship-3">An element that will display the name of the ship docked at gate 3</a>
 		</p>
 
 	</div>
 
-		<label for="shipname">Ship</label>
-		<input type="text" name="ship" id="ship">
 
-		<button id="dock" onclick="setName()">Dock</button>
+	<br>
+	<label for="shipname">Ship</label>
+	<input type="text" name="ship" id="ship">
+	<button id="dock" onclick="setName()">Dock</button>
 
 	<script type="text/javascript">
 
