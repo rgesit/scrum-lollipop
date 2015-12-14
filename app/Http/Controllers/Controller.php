134e5c977ad35a93b40c608c32c3624a63f5c8d9
Heri Gunawan <aageboi@gmail.com>
2015-12-14 13:59:05 +0700

    Update the home page of your server to include a Contact me link.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,5 +6,13 @@ use Laravel\Lumen\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    //
+    public function index()
+    {
+    	return view('index');
+    }
+
+    public function aboutme()
+    {
+    	echo 'About me';
+    }
 }
