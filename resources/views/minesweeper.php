01d5f9b923d99616f147363ad35a858031c39d69
HQM <hakim@detik.com>
2015-12-14 14:36:28 +0700

    Minesweeper 7th try

--- resources/views/minesweeper.php
+++ resources/views/minesweeper.php
@@ -1,31 +1,7 @@
-<html>
-<head>
-	<title>Minesweeper</title>
-</head>
-<body>
-
-	<table border="1">
-		<?php for($i= 1; $i<=8; $i++):?>
-			<tr>
-				<?php for($j= 1; $j<=8; $j++):?>
-				<td
-					<?php if($cell == $i && $row == $j) : ?>
-							id="Minesweeper"			
-					<?php else: ?>
-						 id="cell-<?php echo $i?>x<?php echo $j ?>"
-					<?php endif ?>
-				>
-				
-				<?php if($cell == $i && $row == $j) : ?>
-						<span id="Minesweeper">Minesweeper</span>
-				<?php else:?>
-				&nbsp;
-				<?php endif ?>
-				
-				</td>
-				<?php endfor ?>
-			</tr>
-		<?php endfor ?>
-	</table>
-</body>
-</html>
+<html><body>
+<label id="title">Minesweeper</label>
+							<label id="cell-1x1" class="any"></label>
+							<label id="cell-1x2"></label>
+							<label id="cell-2x1"></label>
+							<label id="cell-2x2"></label>
+							</body></html>
