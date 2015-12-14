4b6cb239b1bd868c36d50a4b57a5c58e8ec15c59
Heri Gunawan <aageboi@gmail.com>
2015-12-14 14:45:23 +0700

    fix gates

--- resources/views/astroport.blade.php
+++ resources/views/astroport.blade.php
@@ -8,12 +8,15 @@
 
 		<h1>Lollipop</h1>
 
-		<p><a href="/astroport/gate-1" id="gate-1">An element that will help the user to identify the first gate</a></p>
-		<p><a href="/astroport/gate-2" id="gate-2">An element that will help the user to identify the second gate</a></p>
-		<p><a href="/astroport/gate-3" id="gate-3">An element that will help the user to identify the third gate</a></p>
-		<p><a href="/astroport/ship-1" id="ship-1">An element that will display the name of the ship docked at gate 1</a></p>
-		<p><a href="/astroport/ship-2" id="ship-2">An element that will display the name of the ship docked at gate 2</a></p>
-		<p><a href="/astroport/ship-3" id="ship-3">An element that will display the name of the ship docked at gate 3</a></p>
+		<p id="gate-1">
+			<a href="/astroport/ship-1" id="ship-1">An element that will display the name of the ship docked at gate 1</a>
+		</p>
+		<p id="gate-2">
+			<a href="/astroport/ship-2" id="ship-2">An element that will display the name of the ship docked at gate 2</a>
+		</p>
+		<p id="gate-3">
+			<a href="/astroport/ship-3" id="ship-3">An element that will display the name of the ship docked at gate 3</a>
+		</p>
 
 	</div>
 </body>
