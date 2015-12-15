bee9b196e9dad805a7b891b75b9caacd174e4470
HQM <hakim@detik.com>
2015-12-15 14:02:29 +0700

    Minesweeper 2nd day :D 6th try

--- resources/views/minesweeper.php
+++ resources/views/minesweeper.php
@@ -2,8 +2,24 @@
 <head>
 	
 	<script type="text/javascript">	
-	function load(){
+	function load(data){
 
+		// data = [
+  //       ['empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
+  //       ['empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
+  //       ['empty', 'empty', 'empty', 'empty', 'empty', 'bomb' , 'empty', 'empty'],
+  //       ['empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
+  //       ['empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
+  //       ['empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
+  //       ['empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
+  //       ['empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
+  //   	];	
+
+    	for (var i =1; i<=8; i++){    		
+    		for(var j=1; j<=8; j++){
+    			document.getElementById("cell-"+i+"x"+j).innerHTML = data[i-1][j-1];
+    		}
+    	}
 	}
 
 	function check(row, cell){
@@ -26,11 +42,15 @@
 <?php for($i = 1; $i<=8; $i++) :?>	
 	<tr>
 	<?php for($j = 1; $j<=8; $j++) :?>
-		<td onclick="check(<?php echo $i ?>, <?php echo $j ?>)"  id="cell-<?php echo $i ?>x<?php echo $j?>" class="lost">
+		<td onclick="check(<?php echo $i ?>, <?php echo $j ?>)"  id="cell-<?php echo $i ?>x<?php echo $j?>" class="">
 		</td>
 	<?php endfor ?>
 	</tr>
 <?php endfor ?>
 </table>
+
+<script type="text/javascript">
+		load();
+</script>
 </body>
 </html>
