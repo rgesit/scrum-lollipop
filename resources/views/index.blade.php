dff12ee2b5e3b24291a620c34c5d70032ec16c4f
Heri Gunawan <aageboi@gmail.com>
2015-12-15 13:22:25 +0700

    share

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,6 +4,7 @@
 	<title></title>
 </head>
 <body>
+	<a href="http://lumen.bangfikri.com/#readme" id="repository-link">YoseTheGame</a><br>
 	<a href="/aboutme" id="contact-me-link">Contact Me</a><br>
 	<a href="/ping" id="ping-challenge-link">Portfolio</a>
 </body>
