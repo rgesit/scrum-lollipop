bea22f11bd705fe0de52c5c4b74d121df8448e1e
rgesit <rgesit@detik.com>
2015-12-15 13:42:15 +0700

    uodate share

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,9 +4,10 @@
 	<title></title>
 </head>
 <body>
-	<a href="http://lumen.bangfikri.com/#readme" id="repository-link">YoseTheGame</a><br>
+	<a href="http://lumen.bangfikri.com/#readme" id="repository-link">Repo</a><br>
 	<a href="/aboutme" id="contact-me-link">Contact Me</a><br>
 	<a href="/ping" id="ping-challenge-link">Portfolio</a>
+	<a href="http://lumen.bangfikri.com/readme.txt">readme</a><br>
 </body>
 </html>
 
